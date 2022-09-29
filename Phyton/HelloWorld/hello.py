f0f5a10b45535cdcaa165d73d48198b09b068689
Gloria-74 <96049276+Gloria-74@users.noreply.github.com>
2022-09-29 10:59:23 +0100

    update new tutorial file

--- Phyton/HelloWorld/hello.py
+++ Phyton/HelloWorld/hello.py
@@ -2,6 +2,8 @@
 # tutorial
 # https://www.techbeamers.com/python-tutorial-step-by-step/#tutorial-list
 
+# more tutorials: https://www.youtube.com/results?search_query=python+beginner+tutorial+deutsch
+
 msg = "Hello World"
 
 # print() is used to print Messages in Python
@@ -36,10 +38,6 @@ print('\nInterest = %0.2f' %interest)
 # / Division -> It divides left-hand operand by right-hand operand.
 # pow(X, Y, Z) -> It determines [X to the power Y]. If Z is available, then it’ll return X to the power Y, modulo Z.
 
-print('Interest Calculator:')
-amount = float(input('Principal amount ?'))
-roi = float(input('Rate of Interest ?'))
-years = int(input('Duration (no. of years) ?'))
-total = (amount * pow(1 + (roi/100), years))
-interest = total - amount
-print('\nInterest = %0.2f' %interest)
+
+# \n is used to print the text in a new line
+# if you use input, it waits for the input you write and then it goes to the next line until you are at the last statement
