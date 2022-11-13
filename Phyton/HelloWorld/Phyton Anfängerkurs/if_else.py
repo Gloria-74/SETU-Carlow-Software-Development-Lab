e5cdb4b81ef9e149dc1cb1aa35eb13a05c5f26c5
Gloria-74 <96049276+Gloria-74@users.noreply.github.com>
2022-11-13 17:18:19 +0000

    update if else

---
+++ Phyton/HelloWorld/Phyton Anfängerkurs/if_else.py
@@ -0,0 +1,20 @@
+age = int(input("Bitte gib dein Alter ein: "))
+
+if age < 18:
+    print("Achtung, der Nutzer ist jünger als 18 Jahre!")
+
+if age < 18:
+    print("Achtung, der Nutzer ist jünger als 18 Jahre!")
+else:
+    print("Der Nutzer ist volljährig!")
+
+print("Programmende")
+
+if age < 18:
+    print("Achtung, der Nutzer ist jünger als 18 Jahre!")
+elif age == 18:
+    print("Der Nutzer ist genau 18 Jahre alt!")
+else:
+    print("Der Nutzer ist volljährig!")
+
+print("Programmende")
