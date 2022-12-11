8989c89750271461eac1bbaae00c5553ddba27d4
Gloria-74 <96049276+Gloria-74@users.noreply.github.com>
2022-12-11 20:45:36 +0000

    upload current Lab 10 - Blackboard was not working

---
+++ Lab10/src/ChangeColourDriver.java
@@ -0,0 +1,19 @@
+// Student Name 	: Gloria Zimmermann
+// Student Id Number: C00290104
+// Date 			: December 2022
+// Purpose 			: Lab Exercise 10
+
+import javax.swing.JFrame;
+
+public class ChangeColourDriver {
+
+    public static void main(String[] args) {
+
+        ChangeColour changeColour = new ChangeColour("Button");
+        changeColour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        changeColour.setSize(200, 200);
+        changeColour.setVisible(true);
+
+    }
+
+}
